327899765e4c6fa36f6c68260bfe5533239822d0
Gabriell David <vacircagabriel21@gmail.com>
2026-05-12 00:53:40 -0300

    feat: adding theme

---
+++ flutter_application_1/lib/core/constants/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  // Brand
+  static const primary = Color(0xFFE8622A);
+  static const primaryLight = Color(0xFFFFF3EE);
+  static const primaryDisabled = Color(0xFFF0997B);
+
+  // Neutral
+  static const background = Color(0xFFF7F6F2);
+  static const surface = Color(0xFFFFFFFF);
+  static const border = Color(0xFFE5E3DC);
+  static const divider = Color(0xFFEDEBE4);
+
+  // Text
+  static const textPrimary = Color(0xFF1A1A1A);
+  static const textSecondary = Color(0xFF888780);
+  static const textHint = Color(0xFFBBB9B2);
+
+  // Misc
+  static const avatarBackground = Color(0xFFE8622A);
+  static const avatarForeground = Color(0xFFFFFFFF);
+}
