f879c1e871a4d94b722a68dfacd6e9c538ee71f5
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:29:34 -0300

    refactor(ui): add onboarding_page_data.dart file

---
+++ flutter_application_1/lib/presentation/screens/onboarding_page_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPageData {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+
+  const OnboardingPageData({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  });
+}
