568778d44a3c05cefc9c652363cea89e93e77c58
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:29:19 -0300

    refactor(ui): add home_shell.dart file

--- flutter_application_1/lib/presentation/screens/home_shell.dart
+++ flutter_application_1/lib/presentation/screens/home_shell.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../core/theme/app_colors.dart';
-import '../../core/theme/theme_scope.dart';
-import '../../core/theme/theme_rebuild.dart';
+
 import '../../domain/models/user_model.dart';
-import '../widgets/bottom_nav_bar.dart';
-import 'tasks_screen.dart';
-import 'progress_screen.dart';
+import 'home_shell_state.dart';
 
 /// Shell de navegação entre as abas autenticadas.
 class HomeShell extends StatefulWidget {
@@ -15,33 +11,5 @@ class HomeShell extends StatefulWidget {
   const HomeShell({super.key, required this.user, required this.onLogout});
 
   @override
-  State<HomeShell> createState() => _HomeShellState();
-}
-
-class _HomeShellState extends State<HomeShell> {
-  NavTab _currentTab = NavTab.tasks;
-
-  @override
-  Widget build(BuildContext context) {
-    return ThemeRebuild(
-      builder: (context) {
-        ThemeScope.watch(context);
-
-        return Scaffold(
-          backgroundColor: AppColors.background,
-          body: IndexedStack(
-            index: _currentTab.index,
-            children: [
-              TasksScreen(user: widget.user, onLogout: widget.onLogout),
-              ProgressScreen(user: widget.user, onLogout: widget.onLogout),
-            ],
-          ),
-          bottomNavigationBar: AppBottomNavBar(
-            currentTab: _currentTab,
-            onTabChanged: (tab) => setState(() => _currentTab = tab),
-          ),
-        );
-      },
-    );
-  }
+  State<HomeShell> createState() => HomeShellState();
 }
