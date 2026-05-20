3c0eaabfddeeb4f7200dbe9de1c6bf9d98af280d
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:32:41 -0300

    chore(macos): register flutter_secure_storage plugin

--- flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import flutter_secure_storage_macos
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
