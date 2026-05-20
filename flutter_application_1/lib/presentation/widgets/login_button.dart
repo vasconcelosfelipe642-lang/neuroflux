e9c8636f77a075e427068a9d5f734b0a8aa3f60a
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:31:08 -0300

    refactor(ui): add login_button.dart file

---
+++ flutter_application_1/lib/presentation/widgets/login_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/app_strings.dart';
+import '../../core/theme/app_text_styles.dart';
+
+class LoginButton extends StatelessWidget {
+  final bool isLoading;
+  final VoidCallback onPressed;
+
+  const LoginButton({super.key, required this.isLoading, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPressed,
+      child: isLoading
+          ? const SizedBox(
+              height: 22,
+              width: 22,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.5,
+                color: Colors.white,
+              ),
+            )
+          : Text(AppStrings.loginButton, style: AppTextStyles.fabLabel),
+    );
+  }
+}
