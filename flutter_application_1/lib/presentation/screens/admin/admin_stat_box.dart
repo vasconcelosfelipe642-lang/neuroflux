86c8870fe230e707b2c6f74e1f3622973993e387
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:28:51 -0300

    refactor(ui): add admin_stat_box.dart file

---
+++ flutter_application_1/lib/presentation/screens/admin/admin_stat_box.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/app_sizes.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_text_styles.dart';
+import '../../../core/theme/theme_scope.dart';
+
+class AdminStatBox extends StatelessWidget {
+  final String value;
+  final String label;
+  final bool highlight;
+
+  const AdminStatBox({
+    super.key,
+    required this.value,
+    required this.label,
+    this.highlight = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeScope.watch(context);
+
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: AppSizes.lg, horizontal: AppSizes.sm),
+      decoration: BoxDecoration(
+        color: highlight ? AppColors.primaryLightTint : AppColors.surface,
+        borderRadius: BorderRadius.circular(AppSizes.radiusMd),
+        border: Border.all(color: AppColors.border),
+      ),
+      child: Column(
+        children: [
+          Text(
+            value,
+            style: highlight
+                ? AppTextStyles.adminHighlightPercent
+                : AppTextStyles.adminStatNumber.copyWith(fontSize: 22),
+          ),
+          const SizedBox(height: AppSizes.xs),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: AppTextStyles.adminStatLabel,
+          ),
+        ],
+      ),
+    );
+  }
+}
