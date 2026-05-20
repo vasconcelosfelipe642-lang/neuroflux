a38671b4a8ac0e510fe4b6c0655481c5c6cc2f07
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:30:44 -0300

    refactor(ui): add bottom_nav_bar.dart file

--- flutter_application_1/lib/presentation/widgets/bottom_nav_bar.dart
+++ flutter_application_1/lib/presentation/widgets/bottom_nav_bar.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+
+import '../../core/constants/app_sizes.dart';
+import '../../core/constants/app_strings.dart';
 import '../../core/theme/app_colors.dart';
 import '../../core/theme/theme_scope.dart';
-import '../../core/theme/app_text_styles.dart';
-import '../../core/constants/app_strings.dart';
-import '../../core/constants/app_sizes.dart';
-
-enum NavTab { tasks, progress }
+import 'bottom_nav_item.dart';
+import 'nav_tab.dart';
 
 class AppBottomNavBar extends StatelessWidget {
   final NavTab currentTab;
@@ -29,55 +29,19 @@ class AppBottomNavBar extends StatelessWidget {
       ),
       child: Row(
         children: [
-          _NavItem(
+          BottomNavItem(
             icon: Icons.check_box_outlined,
             label: AppStrings.navTasks,
             isActive: currentTab == NavTab.tasks,
             onTap: () => onTabChanged(NavTab.tasks),
           ),
-          _NavItem(
+          BottomNavItem(
             icon: Icons.trending_up_rounded,
             label: AppStrings.navProgress,
             isActive: currentTab == NavTab.progress,
             onTap: () => onTabChanged(NavTab.progress),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class _NavItem extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final bool isActive;
-  final VoidCallback onTap;
-
-  const _NavItem({
-    required this.icon,
-    required this.label,
-    required this.isActive,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final color = isActive ? AppColors.primary : AppColors.textHint;
-
-    return Expanded(
-      child: InkWell(
-        onTap: onTap,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, color: color, size: AppSizes.iconMd),
-            const SizedBox(height: 3),
-            Text(
-              label,
-              style: AppTextStyles.navLabel.copyWith(color: color),
-            ),
-          ],
-        ),
       ),
     );
   }
