0b9c6f71ed467054f79ee2b96da29cdbc471a4df
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 17:50:04 -0300

    feat(onboarding): three-page first-run flow with SharedPreferences flag

---
+++ flutter_application_1/lib/core/utils/onboarding_storage.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract final class OnboardingStorage {
+  static const _key = 'onboarding_completed';
+
+  static Future<bool> isCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_key) ?? false;
+  }
+
+  static Future<void> markCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, true);
+  }
+}
