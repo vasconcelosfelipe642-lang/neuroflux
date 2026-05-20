cf52846dd3e82efe5598df68a286dd77045c32f0
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:30:59 -0300

    refactor(ui): add day_progress_card_header.dart file

---
+++ flutter_application_1/lib/presentation/widgets/day_progress_card_header.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/app_sizes.dart';
+import '../../core/constants/app_strings.dart';
+import '../../core/theme/app_text_styles.dart';
+
+class DayProgressCardHeader extends StatelessWidget {
+  const DayProgressCardHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          Icons.track_changes_rounded,
+          color: Colors.white,
+          size: AppSizes.iconMd,
+        ),
+        const SizedBox(width: AppSizes.sm),
+        Text(AppStrings.dayProgress, style: AppTextStyles.bigCardTitle),
+      ],
+    );
+  }
+}
