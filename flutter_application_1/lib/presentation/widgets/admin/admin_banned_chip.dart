4fc81bf2f2265f3aae718cd808957ecd95d43e7c
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:30:15 -0300

    refactor(ui): add admin_banned_chip.dart file

---
+++ flutter_application_1/lib/presentation/widgets/admin/admin_banned_chip.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/app_sizes.dart';
+import '../../../core/constants/app_strings.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_text_styles.dart';
+
+class AdminBannedChip extends StatelessWidget {
+  const AdminBannedChip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: AppColors.muted,
+        borderRadius: BorderRadius.circular(AppSizes.radiusFull),
+      ),
+      child: Text(AppStrings.adminBanned, style: AppTextStyles.adminStatusBanned),
+    );
+  }
+}
