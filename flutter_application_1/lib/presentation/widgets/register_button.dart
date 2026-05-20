4fecd513d1af1800b15c810808dee90f9032713e
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:32:02 -0300

    refactor(ui): add register_button.dart file

---
+++ flutter_application_1/lib/presentation/widgets/register_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/app_strings.dart';
+import '../../core/theme/app_text_styles.dart';
+
+class RegisterButton extends StatelessWidget {
+  final bool isLoading;
+  final VoidCallback onPressed;
+
+  const RegisterButton({super.key, required this.isLoading, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPressed,
+      child: isLoading
+          ? const SizedBox(
+              height: 22,
+              width: 22,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.5,
+                color: Colors.white,
+              ),
+            )
+          : Text(AppStrings.registerButton, style: AppTextStyles.fabLabel),
+    );
+  }
+}
