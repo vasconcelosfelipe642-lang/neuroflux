7153a453b4dd289fcc79a444794cfa7656fc7367
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:30:38 -0300

    refactor(ui): add auth_text_field.dart file

--- flutter_application_1/lib/presentation/widgets/auth_text_field.dart
+++ flutter_application_1/lib/presentation/widgets/auth_text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../core/theme/app_colors.dart';
-import '../../core/theme/app_text_styles.dart';
+
+import 'auth_text_field_state.dart';
 
 /// Campo de texto padronizado para as telas de autenticação.
 /// Reutilizável em Login e Cadastro.
@@ -29,42 +29,5 @@ class AuthTextField extends StatefulWidget {
   });
 
   @override
-  State<AuthTextField> createState() => _AuthTextFieldState();
-}
-
-class _AuthTextFieldState extends State<AuthTextField> {
-  bool _obscure = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(widget.label, style: AppTextStyles.fieldLabel),
-        const SizedBox(height: 8),
-        TextFormField(
-          controller: widget.controller,
-          focusNode: widget.focusNode,
-          obscureText: widget.isPassword && _obscure,
-          keyboardType: widget.keyboardType,
-          textInputAction: widget.textInputAction,
-          validator: widget.validator,
-          onEditingComplete: widget.onEditingComplete,
-          decoration: InputDecoration(
-            hintText: widget.hint,
-            suffixIcon: widget.isPassword
-                ? IconButton(
-                    icon: Icon(
-                      _obscure ? Icons.visibility_off_outlined : Icons.visibility_outlined,
-                      color: AppColors.textHint,
-                      size: 20,
-                    ),
-                    onPressed: () => setState(() => _obscure = !_obscure),
-                  )
-                : null,
-          ),
-        ),
-      ],
-    );
-  }
+  State<AuthTextField> createState() => AuthTextFieldState();
 }
