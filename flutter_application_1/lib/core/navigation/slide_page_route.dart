faf979664ea25fd653acb868799b62ad916b144f
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:28:28 -0300

    refactor(navigation): add SlidePageRoute file

---
+++ flutter_application_1/lib/core/navigation/slide_page_route.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SlidePageRoute<T> extends PageRouteBuilder<T> {
+  SlidePageRoute({required Widget child, RouteSettings? settings})
+      : super(
+          settings: settings,
+          pageBuilder: (context, animation, secondaryAnimation) => child,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(1, 0);
+            const end = Offset.zero;
+            final tween = Tween(begin: begin, end: end)
+                .chain(CurveTween(curve: Curves.easeOutCubic));
+            return SlideTransition(
+              position: animation.drive(tween),
+              child: child,
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 350),
+        );
+}
