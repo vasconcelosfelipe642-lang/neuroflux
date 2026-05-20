bbd75923f96c6cd069d9c528a5476ee11b227336
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 20:28:33 -0300

    refactor(ui): add admin_stats_model.dart file

--- flutter_application_1/lib/domain/models/admin_stats_model.dart
+++ flutter_application_1/lib/domain/models/admin_stats_model.dart
@@ -11,13 +11,3 @@ class AdminStatsModel {
     required this.bannedUsers,
   });
 }
-
-class UserTaskStatsModel {
-  final int created;
-  final int completed;
-
-  const UserTaskStatsModel({required this.created, required this.completed});
-
-  int get completionRatePercent =>
-      created == 0 ? 0 : ((completed / created) * 100).round();
-}
