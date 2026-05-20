ab78825911637448c56b39f7d48913171def8e39
Gabriell David <vacircagabriel21@gmail.com>
2026-05-20 17:50:15 -0300

    test: update widget test for ThemeScope app bootstrap

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_application_1/core/theme/theme_scope.dart';
 import 'package:flutter_application_1/main.dart';
 
 void main() {
   testWidgets('NeuroFlux app inicia', (WidgetTester tester) async {
-    await tester.pumpWidget(const NeuroFluxApp());
+    await tester.pumpWidget(
+      ThemeScope(
+        child: NeuroFluxApp(),
+      ),
+    );
     await tester.pump();
     expect(find.byType(NeuroFluxApp), findsOneWidget);
   });
